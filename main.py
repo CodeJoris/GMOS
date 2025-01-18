a8fafb8d29b7209ef78b8cfcf2fdecabeecf0a87
marekdetven <marekvenkatesh@hotmail.com>
2025-01-18 11:40:51 -0500

    e

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import googlemaps
-with open("data.txt", "r") as file:
-    coefficient = float(file.readline())
 from datetime import datetime
 
 gmaps = googlemaps.Client(key='AIzaSyBw8lINwBQQ9t5tv02oBLwty-Kg6n3iLzQ')
 now = datetime.now()
 depart = input("Where are you ?")
 arrivee = input("Where do you want to go ?")
-output = gmaps.directions(depart,arrivee,mode="walking",departure_time=now)
+
+mode = input("How do you want to get there : walking (W), transit (T), car (C) ?").lower()
+choix="walking"
+if mode == "t" or "transit":
+    choix = "transit"
+    with open("TCoef.txt", "r") as file:
+        coefficient = float(file.readline())
+elif mode == "c" or "car":
+    choix = "car"
+    with open("CCoef.txt", "r") as file:
+        coefficient = float(file.readline())
+else:
+    with open("WCoef.txt", "r") as file:
+        coefficient = float(file.readline())    
+
+
+output = gmaps.directions(depart,arrivee,mode=choix,departure_time=now)
 
 def sec_to_min(ina):
     if ina<3600:
@@ -16,6 +30,14 @@ def sec_to_min(ina):
         no_h=(ina//60)//60
         no_m=(ina//60)%60
         return (str(no_h)+" h "+str(no_m)+ " min")
+    
+def coef_update(inp):
+
+    with open("data.txt", "r") as file:
+        if inp=="y" or inp=="yes":
+            
+            
+
 if output:
     route = output[0]
     leg = route['legs'][0]
@@ -28,11 +50,12 @@ else:
 
 
 
-answer = input("Did you reach your destination faster or slower than expected ? (Y/N)").lower
+answer = input("Did you reach your destination faster or slower than expected ? (Y/N/SAME)").lower
 if answer == "y" or "yes":
-    coefficient-=0.05
+    if 
 elif answer=="n" or "no":
     coefficient+=0.05
+
 
 # Save to a text file
 with open("data.txt", "w") as file:
