6637c24a24c2b83f80d17bb46511d970d4d377fa
marekdetven <marekvenkatesh@hotmail.com>
2025-01-18 11:05:35 -0500

    coefficient

--- main.py
+++ main.py
@@ -34,7 +34,15 @@ output = gmaps.directions(depart,arrivee,mode="walking",departure_time=now)
 if output:
     route = output[0]
     leg = route['legs'][0]
-    estimated=leg['duration']['text']
+    estimated=leg['duration']['text'] #this is the estimated time by the API
     print(f"Estimated Time: ", estimated)
 else:
     print("No directions found!")
+
+coefficient = 1
+
+answer = input("Did you reach your destination faster or slower than expected ? (Y/N)").lower
+if answer == "y" or "yes":
+    coefficient-=0.05
+elif answer=="n" or "no":
+    coefficient+=0.05
